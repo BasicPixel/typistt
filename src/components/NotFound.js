55b70b5659fa840c7a9ea420bd6c71d3c28a3b1a
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-01-27 19:08:58 +0200

    Style up the 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,13 +4,15 @@ const NotFound = () => {
   const location = useLocation();
 
   return (
-    <>
-      <div>404: page not found</div>
-      <p>
+    <div className="text-center">
+      <h1 className="my-4 text-3xl">404: page not found</h1>
+      <p className="text-lg">
         Requested path:
-        <code className="rounded bg-slate-700">{location.pathname}</code>
+        <code className="mx-2 rounded dark:bg-slate-700 bg-slate-400">
+          {location.pathname}
+        </code>
       </p>
-    </>
+    </div>
   );
 };
 
